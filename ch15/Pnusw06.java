00829e67fa71ed2e843c29b099002d8838bc8ac2
cyccc95 <cyccc95@naver.com>
2022-11-23 20:47:17 +0900

    pnusw06

--- ch15/Pnusw06.java
+++ ch15/Pnusw06.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Pnusw06 {
 
@@ -15,6 +16,8 @@ public class Pnusw06 {
 	private Statement stmt;
 	private ResultSet rs;
 	private CallableStatement cs;
+	
+	static Scanner sc = new Scanner(System.in);
 	
 	public Pnusw06() {
 		String Driver = "";
@@ -39,26 +42,118 @@ public class Pnusw06 {
 	}
 	
 	private void select() {
-		
+		int select = 0;
+		while(select != 5) {
+			System.out.print("1.학생 2.학과 3.성적 4.강의 5.종료 || 테이블울 선택하세요 : ");
+			select = sc.nextInt();
+			switch(select) {
+				case 1:
+					try {
+						cs = con.prepareCall("{ call selectTable(" + select + ") }");
+						rs = cs.executeQuery();
+						System.out.println(String.format("%-8s %-10s %-17s %-10s", "학생번호","학생이름","주소","학과"));
+						while(rs.next()) {
+							System.out.println(String.format("%-10s %-10s %-15s %-10s",
+									rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4)));
+						} 
+					} catch(SQLException e) {
+							e.printStackTrace();
+							System.out.println("selectTable 중 예외 발생");
+					}
+					break;
+				case 2:
+					try {
+						cs = con.prepareCall("{ call selectTable(" + select + ") }");
+						rs = cs.executeQuery();
+						System.out.println(String.format("%-10s %-10s", "학과", "학과사무실"));
+						while(rs.next()) {
+							System.out.println(String.format("%-10s %-10s",
+									rs.getString(1),rs.getString(2)));
+						} 
+					} catch(SQLException e) {
+							e.printStackTrace();
+							System.out.println("selectTable 중 예외 발생");
+					}
+					break;
+				case 3:
+					try {
+						cs = con.prepareCall("{ call selectTable(" + select + ") }");
+						rs = cs.executeQuery();
+						System.out.println(String.format("%-10s %-10s %-5s", "학생번호","강좌이름","성적"));
+						while(rs.next()) {
+							System.out.println(String.format("%-10s %-10s %-5s",
+									rs.getInt(1),rs.getString(2),rs.getFloat(3)));
+						} 
+					} catch(SQLException e) {
+							e.printStackTrace();
+							System.out.println("selectTable 중 예외 발생");
+					}
+					break;
+				case 4:
+					try {
+						cs = con.prepareCall("{ call selectTable(" + select + ") }");
+						rs = cs.executeQuery();
+						System.out.println(String.format("%-10s %-10s", "강좌이름","강의실"));
+						while(rs.next()) {
+							System.out.println(String.format("%-10s %-10s",
+									rs.getString(1),rs.getString(2)));
+						} 
+					} catch(SQLException e) {
+							e.printStackTrace();
+							System.out.println("selectTable 중 예외 발생");
+					}
+					break;
+				case 5:
+					System.out.println("종료합니다.");
+					break;
+				default:
+					System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+					break;
+			}
+		}
 	}
 	
 	private void selectAll() {
 		try {
 			cs = con.prepareCall("{ call selectAll() }");
 			rs = cs.executeQuery();
+			System.out.println(String.format("%-5s %-5s %-12s %-10s %-10s %-10s %-10s %-2s",
+					"학생번호","학생이름","주소","학과","학과사무실","강좌이름","강의실","성적"));
 			while(rs.next()) {
-				System.out.printf("%-5s %-5s %-10s %-10s %-10s %-10s %-10s %.1f\n",
+				System.out.println(String.format("%-7s %-5s %-10s %-10s %-10s %-10s %-10s %.1f",
 						rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4)
-						,rs.getString(5),rs.getString(6),rs.getString(7),rs.getFloat(8));
+						,rs.getString(5),rs.getString(6),rs.getString(7),rs.getFloat(8)));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out.println("SelectAll 예외 발생");
+			System.out.println("SelectAll 중 예외 발생");
 		}
 	}
 	
 	private void insert() {
+		int 학생번호;
+		String 학생이름, 주소, 학과, 학과사무실, 강좌이름, 강의실;
+		Float 성적;
+		System.out.println("---입력할 데이터---");
+		System.out.print("학생번호 : "); 학생번호 = sc.nextInt();
+		System.out.print("학생이름 : "); 학생이름 = sc.next();
+		System.out.print("주소 : "); 주소 = sc.next();
+		System.out.print("학과 : "); 학과 = sc.next();
+		System.out.print("학과사무실: "); 학과사무실 = sc.next();
+		System.out.print("강좌이름 : "); 강좌이름 = sc.next();
+		System.out.print("강의실 : "); 강의실 = sc.next();
+		System.out.print("성적 : "); 성적 = sc.nextFloat();
 		
+		String input = String.format("{ call insertData(%d, '%s', '%s', '%s', '%s', '%s', '%s', %f) }",
+				학생번호, 학생이름, 주소, 학과, 학과사무실, 강좌이름, 강의실, 성적);
+		try {
+			cs = con.prepareCall(input);
+			cs.executeQuery();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("insert 중 예외 발생");
+		}
 	}
 	
 	private void update() {
@@ -71,7 +166,34 @@ public class Pnusw06 {
 	
 	public static void main(String[] args) {
 		Pnusw06 scon = new Pnusw06();
-		scon.selectAll();
+		int select = 0;
+		while(select != 6) {
+			System.out.print("1.selectAll 2.select 3.insert 4.update 5.delete 6.종료 || 입력 :");
+			select = sc.nextInt();
+			switch(select) {
+				case 1:
+					scon.selectAll();
+					break;
+				case 2:
+					scon.select();
+					break;
+				case 3:
+					scon.insert();
+					break;
+				case 4:
+					scon.update();
+					break;
+				case 5:
+					scon.delete();
+					break;
+				case 6:
+					System.out.println("종료합니다.");
+					break;
+				default:
+					System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+					break;
+			}
+		}
 	}
 
 }
